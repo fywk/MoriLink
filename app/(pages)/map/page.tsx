f091ccd3fc7839b8e217eb7c8cc191c3c639e06b
Francis Yeong <33077075+fywk@users.noreply.github.com>
2023-04-09 16:43:34 +0800

    Change `/map` to Cloudinary-hosted image

--- app/(pages)/map/page.tsx
+++ app/(pages)/map/page.tsx
@@ -2,8 +2,8 @@ import { Metadata } from "next";
 import Image from "next/image";
 
 import PageLayout from "@/components/PageLayout";
+import { getImageURL } from "@/lib/cloudinary";
 import { island } from "@/lib/config";
-import island_map from "@/public/images/map.jpg";
 
 const TITLE = "Map";
 
@@ -19,14 +19,23 @@ export default async function MapPage() {
       navbarBgClass="bg-[#79d7c5]"
       mainBgClass="bg-[#79d7c5]"
     >
-      <div className="mx-auto flex h-full max-w-2xl flex-col items-center justify-center gap-y-9 px-5 py-10 tracking-tight">
+      <div className="mx-auto flex h-full max-w-xl flex-col items-center justify-center gap-y-9 px-5 pb-10 pt-5 tracking-tight">
         <div className="rounded-sm bg-[#faea4a] px-5 py-2.5 text-xl/none font-bold text-dark-bronze-coin/75 shadow-md sm:text-[1.375rem]/none md:px-6 md:py-3 md:text-2xl/none">
           {island.name}
         </div>
         <Image
-          src={island_map}
+          src={getImageURL("map", {
+            quality: 100,
+            width: 640,
+            height: 535,
+            x: 330,
+            y: 100,
+            crop: "crop",
+          })}
+          width={640}
+          height={535}
           alt=""
-          quality={100}
+          quality={85}
           priority
           draggable={false}
           className="mx-auto w-full"

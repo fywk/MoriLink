fbb5ee4de9f7cea57a1ec13fd74b25dd75c3fe37
Francis Yeong <33077075+fywk@users.noreply.github.com>
2023-04-11 15:53:53 +0800

    Add instant loading state for pages

---
+++ app/(pages)/loading.tsx
@@ -0,0 +1,9 @@
+import LoadingBubbles from "@/components/LoadingBubbles";
+
+export default function Loading() {
+  return (
+    <div className="h-[100dvh]">
+      <LoadingBubbles />
+    </div>
+  );
+}

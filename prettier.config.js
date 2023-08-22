6c2367d48aa96bb25c4180f56d8cc144c883f326
Francis Yeong <33077075+fywk@users.noreply.github.com>
2023-08-23 00:59:45 +0800

    Update prettier config

--- prettier.config.js
+++ prettier.config.js
@@ -1,4 +1,22 @@
+/** @type {import("@ianvs/prettier-plugin-sort-imports").PrettierConfig } */
 module.exports = {
   printWidth: 100,
-  plugins: ["prettier-plugin-tailwindcss"],
+  plugins: ["@ianvs/prettier-plugin-sort-imports", "prettier-plugin-tailwindcss"],
+  // @ianvs/prettier-plugin-sort-imports
+  importOrder: [
+    "",
+    "<THIRD_PARTY_MODULES>",
+    "",
+    "^[@/]/(.*)$",
+    "",
+    "^[./]",
+    "",
+    "<TYPES>",
+    "",
+    "<TYPES>^[@/]/(.*)$",
+    "",
+    "<TYPES>^[./]",
+  ],
+  // prettier-plugin-tailwindcss
+  tailwindAttributes: ["navbarBgClass", "mainBgClass"],
 };

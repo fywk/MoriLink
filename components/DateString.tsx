1d21dc90fe5b09c62d4132a8308bf87034fc883d
Francis Yeong <33077075+fywk@users.noreply.github.com>
2023-09-20 15:30:47 +0800

    Test removing useState and useEffect

--- components/DateString.tsx
+++ components/DateString.tsx
@@ -1,15 +1,7 @@
 "use client";
 
-import { useEffect, useState } from "react";
-
 import dayjs from "@/lib/utils/dayjs";
 
 export default function DateString({ format }: { format: string }) {
-  const [date, setDate] = useState("");
-
-  useEffect(() => {
-    setDate(dayjs().format(format));
-  }, [format]);
-
-  return date;
+  return dayjs().format(format);
 }

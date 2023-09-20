74420185d94d0fbb0a69c6362051a270f2c9bed5
Francis Yeong <33077075+fywk@users.noreply.github.com>
2023-09-20 15:26:47 +0800

    Update `CalendarIcon`

--- components/CalendarIcon.tsx
+++ components/CalendarIcon.tsx
@@ -3,13 +3,13 @@ import DateString from "./DateString";
 export default function CalendarIcon() {
   return (
     <div className="flex aspect-square h-full w-full items-center justify-center">
-      <div className="@container grid aspect-square w-3/5 translate-y-px -rotate-[7deg] grid-rows-[20%_1fr]">
+      <div className="grid aspect-square w-[58%] translate-y-px -rotate-[7deg] grid-rows-[20%_1fr] @container">
         <div className="relative rounded-t-xl bg-red-400">
-          <div className="absolute bottom-1 left-[20%] h-2.5 w-[3px] rounded-full bg-dark-bronze-coin"></div>
-          <div className="absolute bottom-1 right-[20%] h-2.5 w-[3px] rounded-full bg-dark-bronze-coin"></div>
+          <div className="absolute bottom-[7.5cqw] left-[22.5cqw] h-full w-[3px] rounded-full bg-dark-bronze-coin"></div>
+          <div className="absolute bottom-[7.5cqw] right-[22.5cqw] h-full w-[3px] rounded-full bg-dark-bronze-coin"></div>
         </div>
         <div className="flex h-full items-center justify-center rounded-b-xl bg-white">
-          <span className="translate-y-px text-[70cqw]/none font-[750] tracking-tighter">
+          <span className="text-[70cqw]/none font-[750] tracking-tighter">
             <DateString format="D" />
           </span>
         </div>

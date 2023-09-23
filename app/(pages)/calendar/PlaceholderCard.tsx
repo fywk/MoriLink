065cbc1bd58156231e2c01775b63814f0c19edf6
Francis Yeong <33077075+fywk@users.noreply.github.com>
2023-09-24 00:53:20 +0800

    Refactor

--- app/(pages)/calendar/PlaceholderCard.tsx
+++ app/(pages)/calendar/PlaceholderCard.tsx
@@ -1,13 +1,13 @@
 import Image from "next/image";
 
+import spriteTownIsland from "@/public/images/sprites/Town_Island.png";
+
 export default function PlaceholderCard() {
   return (
     <div className="relative flex items-center justify-center overflow-hidden rounded-sm bg-[#f0ecd7] font-bold leading-none tracking-tight text-beaver @container">
-      <span className="hidden @[200px]:block">Developing...</span>
+      <p className="hidden @[200px]:block">Developing...</p>
       <Image
-        src="/images/sprites/Town_Island.png"
-        width={128}
-        height={128}
+        src={spriteTownIsland}
         alt=""
         unoptimized
         className="absolute -bottom-4.5 right-0.5 h-[3.25rem] w-[3.25rem] mix-blend-plus-lighter"

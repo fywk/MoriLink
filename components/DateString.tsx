5357d7f2c58f2964406a4017d8dff2f754d479fe
Francis Yeong <33077075+fywk@users.noreply.github.com>
2023-09-25 17:07:06 +0800

    Fix incorrect time when page loads

--- components/DateString.tsx
+++ components/DateString.tsx
@@ -1,7 +1,15 @@
 "use client";
 
+import { useEffect, useState } from "react";
+
 import dayjs from "@/lib/utils/dayjs";
 
 export default function DateString({ format }: { format: string }) {
-  return dayjs().format(format);
+  const [date, setDate] = useState("");
+
+  useEffect(() => {
+    setDate(dayjs().format(format));
+  }, [format]);
+
+  return date;
 }

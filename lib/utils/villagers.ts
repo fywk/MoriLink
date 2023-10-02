2bcf05339704974b0cf4b114a82929e022d19b27
Francis Yeong <33077075+fywk@users.noreply.github.com>
2023-10-02 13:56:26 +0800

    Rename `get-villager.ts` to `villagers.ts`

--- lib/utils/villagers.ts
+++ lib/utils/villagers.ts
@@ -1,7 +1,9 @@
 import villagers from "animal-crossing/lib/data/Villagers.json";
 
-type Villager = (typeof villagers)[number];
+export type Villager = (typeof villagers)[number];
 
 export function getVillager(name: string): Villager | undefined {
   return villagers.find((villager) => villager.name.toLowerCase() === name.toLowerCase());
 }
+
+export default villagers;
